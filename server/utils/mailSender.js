2fa5934e5cfe4798033d1b932aefc1fc685a144f
Pankaj Singh <105196369+Pankajs53@users.noreply.github.com>
2025-02-13 20:21:08 +0530

    Update mailSender.js

--- server/utils/mailSender.js
+++ server/utils/mailSender.js
@@ -45,7 +45,7 @@ const mailSender = async (email, title, body) => {
 
 
             let info = await transporter.sendMail({
-                from: 'StudyNotion || CodeHelp - by Babbar',
+                from: 'StudyNotion || CodeHelp - by Pankaj',
                 to:`${email}`,
                 subject: `${title}`,
                 html: `${body}`,
